0915018ef4072b8cf44f4c8bd5bf0279f3ab12c4
Paul Strong <lapluviosilla@gmail.com>
2024-10-02 13:34:04 -0600

    Fixed task updating, and added a custom esbuild plugin to be compatible with Amplenote

--- esbuild.js
+++ esbuild.js
@@ -1,7 +1,31 @@
 import dotenv from "dotenv";
 import esbuild from "esbuild";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
+
+// Custom plugin to modify the output
+const customPlugin = {
+  name: "custom-output-plugin",
+  setup(build) {
+    build.onEnd(async () => {
+      const outfilePath = path.resolve("build/compiled.js");
+
+      // Read the content of the outfile
+      let modifiedOutput = fs.readFileSync(outfilePath, "utf-8");
+
+      // Modify the output content as needed
+      modifiedOutput = modifiedOutput.replace(
+        /var plugin_default = plugin;\s*\}\)\(\);/,
+        "var plugin_default = plugin;\nreturn plugin;\n})()"
+      );
+
+      // Write the modified output back to the file system
+      fs.writeFileSync(outfilePath, modifiedOutput);
+    });
+  },
+};
 
 const result = await esbuild.build({
   entryPoints: [`lib/plugin.js`],
@@ -9,6 +33,7 @@ const result = await esbuild.build({
   format: "iife",
   outfile: "build/compiled.js",
   platform: "node",
+  plugins: [customPlugin],
   write: true,
 });
 console.log("Result was", result);
